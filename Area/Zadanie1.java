36c0cb04c9ee942b4abca014751d6edd17fe0fea
Artur Iwicki <iwicki.artur@svgames.pl>
2020-01-18 06:17:08 +0100

    Add the Area program body

---
+++ Area/Zadanie1.java
@@ -0,0 +1,35 @@
+package Area;
+
+import java.util.Scanner;
+
+public class Zadanie1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Wybierz figure: 1) Prostokat; 2) Trojkat; 3) Kolo");
+		int wybor = sc.nextInt();
+		
+		switch(wybor) {
+			case 1:
+				System.out.println("Podaj dlugosc pierwszego boku:");
+				int bokA = sc.nextInt();
+				System.out.println("Podaj dlugosc drugiego boku:");
+				int bokB = sc.nextInt();
+				
+				Rectangle r = new Rectangle(bokA, bokB);
+				System.out.println("Pole prostokata: " + r.getArea());
+				System.out.println("Obwod prostokata: " + r.getPerimeter());
+			break;
+			
+			case 2:
+			break;
+			
+			case 3:
+			break;
+		}
+	}
+}
+
+
+
+
