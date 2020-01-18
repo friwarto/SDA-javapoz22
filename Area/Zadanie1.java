054e2ff976b7b79d52edc6ef28bbfa76a0e89617
Artur Iwicki <iwicki.artur@svgames.pl>
2020-01-18 06:21:37 +0100

    Repeat prompts instead of killing the program in Area

--- Area/Zadanie1.java
+++ Area/Zadanie1.java
@@ -6,43 +6,42 @@ public class Zadanie1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Wybierz figure: 1) Prostokat; 2) Trojkat; 3) Kolo");
-		int wybor = sc.nextInt();
-
 		Shape shape = null;
-		switch(wybor) {
-			case 1:
-				System.out.println("Podaj dlugosc pierwszego boku:");
-				int bokA = sc.nextInt();
-				System.out.println("Podaj dlugosc drugiego boku:");
-				int bokB = sc.nextInt();
+		while(shape == null) {
+			System.out.println("Wybierz figure: 1) Prostokat; 2) Trojkat; 3) Kolo");
+			int wybor = sc.nextInt();
+
+			switch(wybor) {
+				case 1:
+					System.out.println("Podaj dlugosc pierwszego boku:");
+					int bokA = sc.nextInt();
+					System.out.println("Podaj dlugosc drugiego boku:");
+					int bokB = sc.nextInt();
+					
+					shape = new Rectangle(bokA, bokB);
+				break;
 				
-				shape = new Rectangle(bokA, bokB);
-			break;
-			
-			case 2:
-				System.out.println("Podaj dlugosc boku:");
-				int side = sc.nextInt();
+				case 2:
+					System.out.println("Podaj dlugosc boku:");
+					int side = sc.nextInt();
+					
+					shape = new Triangle(side);
+				break;
 				
-				shape = new Triangle(side);
-			break;
-			
-			case 3:
-				System.out.println("Podaj promien kola:");
-				int radius = sc.nextInt();
+				case 3:
+					System.out.println("Podaj promien kola:");
+					int radius = sc.nextInt();
+					
+					shape = new Circle(radius);
+				break;
 				
-				shape = new Circle(radius);
-			break;
-			
-			default:
-				System.out.println("Nieprawidlowy wybor!");
-				System.exit(1);
+				default:
+					System.out.println("Nieprawidlowy wybor!");
+			}
 		}
 		
-		if(shape != null) {
-			System.out.println("Pole: " + shape.getArea());
-			System.out.println("Obwod: " + shape.getPerimeter());
-		}
+		System.out.println("Pole: " + shape.getArea());
+		System.out.println("Obwod: " + shape.getPerimeter());
 	}
 }
 
