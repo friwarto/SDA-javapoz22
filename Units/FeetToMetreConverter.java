62e38b73f416ae2d8c1bd6ec39c7c3669481a88a
bartekpoznaniak <bpoznan@amu.edu.pl>
2020-01-18 10:01:27 +0100

    create  class FootToMetreConvertion with methods

--- Units/FeetToMetreConverter.java
+++ Units/FeetToMetreConverter.java
@@ -4,21 +4,21 @@ public class FeetToMetreConverter extends Converter {
 
     @Override
     public String getPrimaryUnit() {
-        return null;
+        return "foot";
     }
 
     @Override
     public String getSecondaryUnit() {
-        return null;
+        return "meter";
     }
 
     @Override
     public double convert(double value) {
-        return 0;
+        return 0.3048*value;
     }
 
     @Override
     public double inverseConvert(double value) {
-        return 0;
+        return 3.2808399*value;
     }
 }
