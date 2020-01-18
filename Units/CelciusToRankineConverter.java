7d0f8d1b69ce25f56a7875edbab8eab0d6200215
Marek <marek.kajetan.hetman@gmail.com>
2020-01-18 10:44:34 +0100

    Celcjusz na Rankiny pkt. 11
    - poprawione 1.1

--- Units/CelciusToRankineConverter.java
+++ Units/CelciusToRankineConverter.java
@@ -10,25 +10,14 @@ public class CelciusToRankineConverter extends Converter {
 		return "Rankine";
 	}
 
-	public static double round(double value, int places) {
-		if (places < 0) throw new IllegalArgumentException();
-
-		long factor = (long) Math.pow(10, places);
-		value = value * factor;
-		long tmp = Math.round(value);
-		return (double) tmp / factor;
-	}
 
 	public double convert(double value) {
-		double not_rounded = (value + 273.15)*9/5;
-		return round(not_rounded, 2);
+		return (value + 273.15)*9/5;
 	}
 
 
 	public double inverseConvert(double value) {
-		double not_rounded = value /9.5 - 273.5;
-		return round(not_rounded,2);
+		return value /9.5 - 273.5;
 	}
-
 }
 
