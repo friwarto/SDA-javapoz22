4f0afbaa6c2c1577ff85bd5a67174141748c1860
bartekpoznaniak <bpoznan@amu.edu.pl>
2020-01-18 09:54:33 +0100

    create empty class with empty methods

---
+++ Units/FeetToMetreConverter.java
@@ -0,0 +1,24 @@
+package Units;
+
+public class FeetToMetreConverter extends Converter {
+
+    @Override
+    public String getPrimaryUnit() {
+        return null;
+    }
+
+    @Override
+    public String getSecondaryUnit() {
+        return null;
+    }
+
+    @Override
+    public double convert(double value) {
+        return 0;
+    }
+
+    @Override
+    public double inverseConvert(double value) {
+        return 0;
+    }
+}
