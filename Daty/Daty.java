08b95d3dcac46ef5d2adef67788e61dbd580e10f
Artur Iwicki <iwicki.artur@svgames.pl>
2020-01-18 06:28:53 +0100

    Add a program that prints the current date

---
+++ Daty/Daty.java
@@ -0,0 +1,14 @@
+package Daty;
+
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.time.Period;
+
+import java.util.Scanner;
+
+public class Daty {
+	public static void main(String[] args) {
+		LocalDate date = LocalDate.now();
+		System.out.println(date);
+	}
+}
